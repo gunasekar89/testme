e3f3805f95c2c7999d1d83fc012f225ccbbcf846
gunasekar.mathiyalagan <gunasekar.mathiyalagan@42-q.com>
2019-07-18 18:41:19 +0530

    testme

--- init-agent-instance.sh
+++ init-agent-instance.sh
@@ -1,14 +1,3 @@
-provider "aws" {
-  region = "eu-west-2"
-}
-
-
-resource "aws_instance" "web_server" {
-ami = "ami-0957378a34aff1ea7"
-instance_type = "t2.small"
-vpc_security_group_ids = ["sg-89a846e0"]
-subnet_id = "subnet-73331f39"
-user_data = <<-EOF
 #!/bin/bash
 sudo yum install -y java-1.8.0-openjdk-devel.x86_64
 sudo wget http://mirrors.estointernet.in/apache/tomcat/tomcat-7/v7.0.94/bin/apache-tomcat-7.0.94.zip
@@ -21,9 +10,3 @@ sed -e '41i<role rolename=\"manager-gui\"/>' -i /opt/apache-tomcat-7.0.94/conf/t
 sed -e '42i<user username=\"admin\" password=\"admin\" roles=\"manager-gui\"/>' -i /opt/apache-tomcat-7.0.94/conf/tomcat-users.xml
 ./shutdown.sh
 ./startup.sh
-EOF
-
-tags = {
-Name = "Web-Server"
-}
-}
